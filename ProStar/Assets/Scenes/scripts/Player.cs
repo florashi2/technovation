141852c9f90e6fa1d7dcc506f79860b0ce8f61d1
florashi2 <florashi24@gmail.com>
2022-03-09 20:46:27 -0500

    wrote the script
    script to move up and down the Player

--- ProStar/Assets/Scenes/scripts/Player.cs
+++ ProStar/Assets/Scenes/scripts/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Vector2 targetPos;
+    public float yincrement;  
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,15 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow) )
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y + yincrement);
+            transform.position = targetPos;
+        } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            targetPos = new Vector2(transform.position.x, transform.position.y - yincrement);
+            transform.position = targetPos;
+        }
+            
+            
     }
 }
