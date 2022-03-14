3acdb66b8e1bfad600f3e82b9cbfc4876da38a55
Drusyaa <hallo.drus@gmail.com>
2022-03-13 20:00:54 -0400

    mouvement of the character and obstacle 1
    Improve the motions of the character, add restrictions, script for obstacle 1, life count

---
+++ ProStar/Assets/Scenes/scripts/Obstacle.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public int damage = 1;
+    public float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left* speed* Time.deltaTime);
+}
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+      if (other.CompareTag("Player"))
+        {
+            // player takes damage!
+            other.GetComponent<Player>().health -= damage;
+            Debug.Log(other.GetComponent<Player>().health);
+            Destroy(gameObject);
+           
+        }
+    }
+}
--- ProStar/Assets/Scenes/scripts/Player.cs
+++ ProStar/Assets/Scenes/scripts/Player.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Vector2 targetPos;
-    public float yincrement;  
+    public float yincrement;
+    public float speed;
+    public float maxHeight;
+    public float minHeight;
+    public int health = 3;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,15 +19,12 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed* Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight)
         {
-            targetPos = new Vector2(transform.position.x, transform.position.y + yincrement);
-            transform.position = targetPos;
-        } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
-            targetPos = new Vector2(transform.position.x, transform.position.y - yincrement);
-            transform.position = targetPos;
+            targetPos = new Vector2(transform.position.x, transform.position.y + yincrement);  
+        } else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight) {
+            targetPos = new Vector2(transform.position.x, transform.position.y - yincrement); 
         }
-            
-            
     }
 }
